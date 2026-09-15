1343d89d02d82bbfc975056e96e5288b7aba67e8
Yu <lunari8546@gmail.com>
2026-09-16 01:14:47 +1000

    fix: auth exclude docs

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -72,7 +72,7 @@ export default defineNuxtConfig({
     redirectOptions: {
       login: "/auth",
       callback: "/auth/callback",
-      exclude: ["/", "/docs"],
+      exclude: ["/", "/docs*", "/legal/*"],
       saveRedirectToCookie: true,
     },
   },
